8145d8ebe5f9f0a28c22f657df4c0a313bd6acb8
martapien <marta.pienkowska@earth.ox.ac.uk>
2016-01-08 13:52:41 +0000

    Testing method that counts rings

---
+++ tests/count_rings_test.py
@@ -0,0 +1,23 @@
+import numpy as np
+import count_rings as cr
+
+
+def count_test_1():
+    test_array = np.array([0, 1])
+    exp_count = 1
+    obs_count = cr.count_rings(test_array)
+    assert (exp_count == obs_count)
+
+    
+def count_test_2():
+    test_array = np.array([0, 1, 0, 1, 0, 1])
+    exp_count = 3
+    obs_count = cr.count_rings(test_array)
+    assert (exp_count == obs_count)
+
+
+def count_test_3():
+    test_array = np.array([0, 1, 0, 0, 0, 1])
+    exp_count = 2
+    obs_count = cr.count_rings(test_array)
+    assert (exp_count == obs_count)
