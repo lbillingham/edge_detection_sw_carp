08aab1826b5f91895fd121ee02d6f1828b7f300e
martapien <marta.pienkowska@earth.ox.ac.uk>
2016-01-08 13:15:03 +0000

    Test detection of single edge of a circle

--- tests/detect_edge_test.py
+++ tests/detect_edge_test.py
@@ -1,17 +1,19 @@
+#NOTE:
+#verify edge_detection and edge_test module and fn names
+#verify count_rings module and fn name
+
 import numpy as np
-import matplotlib.pyplot as plt
 from scipy import ndimage as ndi
 
 #input correct module name here
-from detect_edge import * 
-
+from edge_detection import detect_edge
+from count_rings import count_rings
+from pick_profile import pick_profile
 
 def generate_image(st_dev_gauss=0, noise_param=0):
-    
     img = np.zeros((200, 200))
     rr, cc = circle(100, 100, 50)
     img[rr, cc] = 1
-
     img = ndi.gaussian_filter(im, st_dev_gauss)
     img += noise_param * np.random.random(im.shape)
     return img
@@ -19,21 +21,24 @@ def generate_image(st_dev_gauss=0, noise_param=0):
 
 def test_clean_img():
     im = generate_image()
-    edge_test = detect_edge(im, sigma=2) 
-    
-    assert (count_rings == 1)
+    edge_test = detect_edge(im, sigma=2)
+    profile = pick_profile(edge_test) 
+    ring_number = count_rings(profile)
+    assert (ring_number == 1)
 
 
 def test_medium_noise_img():
     im = generate_image(2, 0.2)
     edge_test = detect_edge(im, sigma=3)    
-
-    assert (count_rings == 1)
+    profile = pick_profile(edge_test) 
+    ring_number = count_rings(profile)
+    assert (ring_number == 1)
     
         
 def test_high_noise_img():
     im = generate_image(4, 0.5)
     edge_test = detect_edge(im, sigma=6)
-                                           
-    assert (count_rings == 1)
+    profile = pick_profile(edge_test) 
+    ring_number = count_rings(profile)                                     
+    assert (ring_number == 1)
 
