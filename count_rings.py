414a450d53869dcb4bf5795c6da0dccbc6dbc4c9
Ronan McAdam <Ronan McAdam>
2016-01-08 14:34:43 +0000

    count_rings.py assertion

--- count_rings.py
+++ count_rings.py
@@ -1,11 +1,11 @@
 def count_rings(array):
-	"""
-	Input: Line (as array) through 2D edge array
-	Output: Number of rings (not edges)
-	"""
-	assert type(array) is np.ndarray
-	count=0
-	for i in range(array.size-1):
-		if array[i]==1 and array[i+1]==0:
+    """
+    Input: Line (as array) through 2D edge array
+    Output: Number of rings (not edges)
+    """
+    assert type(array) is np.ndarray
+    count=0
+    for i in range(array.size-1):
+        if array[i]==1 and array[i+1]==0:
 			count=count+1
 	return count
