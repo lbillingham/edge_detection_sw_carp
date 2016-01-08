b995dea44c534286369bb6426af91b069ad9cf81
Beth Woodhams <eebjw@leeds.ac.uk>
2016-01-08 13:21:52 +0000

    Comments added to plot_edge and call_file

--- call_file.py
+++ call_file.py
@@ -2,6 +2,11 @@ from tkinter import *
 from tkinter import filedialog
 
 def call_file():
+    '''
+    User chooses image from pop-up and filepath is returned
+    
+    Note, pop-up currently appears behind browser
+    '''
     root = Tk()
     root.withdraw()
     file_path = filedialog.askopenfilename(title='Choose an image')
