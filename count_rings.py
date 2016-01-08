0ff6e26703652369e8fa1789d65cc7ed82207047
Ronan McAdam <Ronan McAdam>
2016-01-08 14:08:29 +0000

    improved count_rings.py

--- count_rings.py
+++ count_rings.py
@@ -3,12 +3,11 @@ def count_rings(array):
 	Input: Line (as array) through 2D edge array
 	Output: Number of rings (not edges)
 	"""
+	assert (type(array)=
 	count=0
 	for i in range(array.size-1):
 		if array[i]==1 and array[i+1]==0:
 			count=count+1
-	if (count%2)==1:
-		count=count+1
 	return count
 	
 	
