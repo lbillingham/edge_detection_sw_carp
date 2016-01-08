5dcfe6eb96942a74d4dcb4e011a991d473e4f1e0
Beth Woodhams <eebjw@leeds.ac.uk>
2016-01-08 13:33:14 +0000

    test3 delete, plot_edge updated

--- plot_edge.py
+++ plot_edge.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+from edge_detection import detect_edge
+
 def plot_edge(im,sigma=1,high_threshold,low_threshold):
     '''
     Plots original image and edge image next to each other.
@@ -13,12 +16,20 @@ def plot_edge(im,sigma=1,high_threshold,low_threshold):
 
     edges = detect_edge(im,sigma,high_threshold,low_threshold)
     
-    fig = plt.figure(figsize=(10,3))
-    ax1 = fig.add_subplot(1,2,1)
-    ax2 = fig.add_subplot(1,2,2)
+
+    fig, axes = plt.subplots(nrows=1, ncols=2)
+    ax_orig = axes[0]
+    ax_edges = axes[1]
+    ax_orig.imshow(image_in)
+    ax_edges.imshow(edges)
+    fig.show()
+
+#    fig = plt.figure(figsize=(10,3))
+#    ax1 = fig.add_subplot(1,2,1)
+#    ax2 = fig.add_subplot(1,2,2)
     
-    ax1.imshow(im)
-    ax2.imshow(edges)
+#    ax1.imshow(im)
+#    ax2.imshow(edges)
     
-    fig.tight_layout()
-    plt.show()
+#    fig.tight_layout()
+#    fig, axes = plt.subplots(nrows=1, ncols=2)
